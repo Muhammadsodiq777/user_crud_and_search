9d171405e21cad74f81fc958672a427e7e937b96
Muhammadsodiq777 <m.muhammadsodiq777@gmail.com>
2024-04-04 12:27:46 +0500

    some modifications

---
+++ src/main/java/org/zulfiqor/user_crud_and_search/consts/Locale.java
@@ -0,0 +1,7 @@
+package org.zulfiqor.user_crud_and_search.consts;
+
+public interface Locale {
+    String EXIST_USER_PHONE = "exist-user-phone";
+    String PHONE_NUMBER_FORMAT = "phoneNumber-format";
+    String NOT_FOUND_EMPLOYEE = "not-found-employee";
+}
